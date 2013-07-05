1813b6d02f3636b308c1058fd37c48e87a00c299
Merlyn-Cooper <merlyn.cooper@hotmail.co.uk>
2013-07-05 11:16:13 +0100

    testing pendingInsertion

--- tests/src/SclNominetEpp/Response/Info/HostTest.php
+++ tests/src/SclNominetEpp/Response/Info/HostTest.php
@@ -46,8 +46,8 @@ class HostTest extends \PHPUnit_Framework_TestCase
         xsi:schemaLocation="urn:ietf:params:xml:ns:host-1.0 host-1.0.xsd">
         <host:name>ns1.caliban-scl.sch.uk.</host:name>
         <host:roid>739787E8A10BF2CA11882CE974FD775E-UK</host:roid>
-        <host:status s="pendingTransfer"/>
         <host:status s="ok"/>
+        <host:status s="pendingTransfer"/>
         <host:addr ip="v4">1.2.3.4</host:addr>
         <host:clID>UNKNOWN</host:clID>
         <host:crID>NOMINET</host:crID>
@@ -65,8 +65,8 @@ EOX;
 
         $expected = new Nameserver('caliban-scl.sch.uk');
         $expected->setHostName('ns1.caliban-scl.sch.uk.');
-        $expected->addStatus('pendingTransfer');
         $expected->addStatus('ok');
+        $expected->addStatus('pendingTransfer');
         $expected->setIpv4('1.2.3.4');
         $expected->setClientID('UNKNOWN');
         $expected->setCreatorID('NOMINET');
